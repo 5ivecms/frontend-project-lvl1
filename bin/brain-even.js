13b61a1644f201e0305d947c5e4aac3f35a024ae
Dmitry Parshin <greatseo@yandex.ru>
2021-01-12 14:39:21 +0300

    fix no-unneeded-ternary

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,6 +2,14 @@
 
 import * as engine from '../src/index.js';
 
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    return true;
+  }
+
+  return false;
+};
+
 const brainEven = () => {
   const quiz = engine.default;
   const questions = [];
@@ -9,8 +17,7 @@ const brainEven = () => {
 
   for (let i = 0; i < engine.questionsLimit; i += 1) {
     const randomInt = engine.getRandomInt(1, 100);
-    const isEven = randomInt % 2 === 0 ? true : false;
-    const rightAnswer = isEven ? 'yes' : 'no';
+    const rightAnswer = isEven(randomInt) ? 'yes' : 'no';
     questions.push([`${engine.questionPrefix}${randomInt}`, rightAnswer]);
   }
 
